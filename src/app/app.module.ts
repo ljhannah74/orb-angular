da3661a3d0eb16d71f7bbc861ebe917efe83b9d2
Lewis Hannah <you@example.com>
2022-11-17 11:19:37 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
